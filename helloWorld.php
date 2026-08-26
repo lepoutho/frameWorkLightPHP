05c716b4548a60819bf721813b8a7d0bc7ebab56
lepoutho <lepoutho@yahoo.fr>
2026-08-26 15:09:24 +0200

    fichiers restants

---
+++ helloWorld.php
@@ -0,0 +1,12 @@
+<?php
+echo "<strong>Hello, World!!</br></strong>";
+// Test de connexion à la base de données
+// (config MAMP : root/root, port MySQL 8889)
+$mysqli = new mysqli('localhost', 'root', 'root', 'app_db', 8889);
+if ($mysqli->connect_error) {
+    die('Erreur de connexion : ' . $mysqli->connect_error);
+}
+
+
+echo "\nConnexion à la base de données réussie,  bien!";
+$mysqli->close();
